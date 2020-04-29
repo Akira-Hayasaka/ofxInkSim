cd8dc338a41bcd6d45299945d746659fe311135e
Kota Onaga <kohtaonaga@gmail.com>
2020-04-29 17:36:19 +0900

    Fix window settings for oF 0.11.0.

--- ofxInkSimFillBufferExample/src/main.cpp
+++ ofxInkSimFillBufferExample/src/main.cpp
@@ -2,18 +2,14 @@
 #include "ofApp.h"
 
 //========================================================================
-int main( )
+int main()
 {
     ofGLFWWindowSettings settings;
     settings.setGLVersion(3, 2);
-    settings.width = 1024;
-    settings.height = 1024;
-    ofCreateWindow(settings);			// <-------- setup the GL context
-
+    settings.setSize(1024, 1024);
+    ofCreateWindow(settings);
     // this kicks off the running of my app
     // can be OF_WINDOW or OF_FULLSCREEN
     // pass in width and height too:
-    ofRunApp( new ofApp());
-
+    ofRunApp(new ofApp());
 }
-
